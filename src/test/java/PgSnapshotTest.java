88e41f1262b15168f30f7780cd64b78490dd26df
provotor <alex.provotor@mail.ru>
2020-03-19 20:05:34 +0300

    Simple unit test to get entities

---
+++ src/test/java/PgSnapshotTest.java
@@ -0,0 +1,20 @@
+import base.SpringTest;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import provotor.petprojects.pgdnapshotdata.PgSnapshotRepository;
+
+public class PgSnapshotTest extends SpringTest {
+    @Autowired
+    private PgSnapshotRepository repository;
+
+    @Test
+    public void getNode() throws Exception {
+        Assert.assertTrue(repository.getNodes(0, 100).getContent().size() > 0);
+    }
+
+    @Test
+    public void getWays() throws Exception {
+        Assert.assertTrue(repository.getWays(0, 100).getContent().size() > 0);
+    }
+}
