ed500df3b425bf4b92214a0a23d688be98123fac
provotor <alex.provotor@mail.ru>
2020-03-19 20:04:39 +0300

    Component for getting all entities

---
+++ src/main/java/provotor/petprojects/pgdnapshotdata/PgSnapshotRepository.java
@@ -0,0 +1,31 @@
+package provotor.petprojects.pgdnapshotdata;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import provotor.petprojects.pgdnapshotdata.repositories.NodesRepository;
+import provotor.petprojects.pgdnapshotdata.repositories.WaysRepository;
+
+import javax.persistence.EntityManagerFactory;
+
+@Component
+public class PgSnapshotRepository {
+    private final NodesRepository nodesRepository;
+    private final WaysRepository waysRepository;
+
+    public PgSnapshotRepository(EntityManagerFactory emf) {
+        JpaRepositoryFactory jpaRepositoryFactory = new JpaRepositoryFactory(emf.createEntityManager());
+        nodesRepository = jpaRepositoryFactory.getRepository(NodesRepository.class);
+        waysRepository = jpaRepositoryFactory.getRepository(WaysRepository.class);
+    }
+
+    public Page<Way> getWays(Integer offset, Integer limit) {
+        return waysRepository.findAll(PageRequest.of(offset, limit));
+    }
+
+    public Page<Node> getNodes(Integer offset, Integer limit) {
+        return nodesRepository.findAll(PageRequest.of(offset, limit));
+    }
+}
